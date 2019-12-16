e172bbd0a6de1ad351aefb87b867207d548a0f17
fabio <fabiovianaqueiroz@gmail.com>
2019-12-16 00:04:18 +0000

    postfix debug 2

--- src/PostfixCalculator.java
+++ src/PostfixCalculator.java
@@ -39,31 +39,76 @@ public class PostfixCalculator
     		Scanner eqScanner = new Scanner(input);
         	
         	while(eqScanner.hasNextLine())
-        	{
-        		String equation = eqScanner.nextLine().trim();
+        	{       		
+        		Object valueTop;
+				Object operator;
+				Object valueBottom;
+				String equation = eqScanner.nextLine().trim();
+				ArrayLinearList numbers = new ArrayLinearList(10);
+				ArrayLinearList operators = new ArrayLinearList(10);
         		
         		for (Character c : equation.toCharArray()) 
         		{
 					// Consider only what's inside the brackets
 					if(c != '(' && c != ')')
-					{
+					{										
 						// Push the operators into their stack
 						if (c == '*' | c == '/' | c == '+') 
 						{
-							System.out.println("operator:" + c);
+							operatorStack.push(c);
 						} 
 						
 						// Push the operands into their stack
 						else 
 						{
-							System.out.println("operand:" + c);
+							operandStack.push(c);
+						}																
+					}
+					
+					// If a right bracket it found pop the last two values in the stack
+					if(c == ')')
+					{
+						valueTop = operandStack.pop();
+						operator = operatorStack.pop();
+						valueBottom = operandStack.pop();
+						
+						// **** Concatenate and push the operands into an array/stack ****
+						numbers.add(0, valueBottom.toString() + valueTop.toString());
+						operandStack.push(valueBottom.toString() + valueTop.toString());
+//						numbers.add(1, valueTop.toString());
+						
+						// Add the operator to an array
+						operators.add(0, operator.toString());
+
+						equation = valueBottom.toString() + valueTop.toString() + operator.toString();
+						
+					}
+					
+					else
+					{
+		
+						//  Add the operator to an array
+						if (c == '*' | c == '/' | c == '+') 
+						{
+//							operator = operatorStack.pop();
+//							operators.add(1, operator.toString());
+//							System.out.println(operator.toString());
 						}
-												
+						
+						else
+						{
+//							valueTop = operandStack.pop();
+//							numbers.add(2, valueTop.toString());
+						}
 					}
         			
 				} 			        		    		
         		
         		System.out.println( "output = " + equation);
+        		System.out.println(numbers.outputList());
+        		System.out.println(operators.outputList());
+        		System.out.println("top operand: " + operandStack.peek());
+        		System.out.println("top operator: " + operatorStack.peek());
         	}   
         	    	
         	eqScanner.close();
