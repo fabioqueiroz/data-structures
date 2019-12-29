cd095d12822d17e7103419864f5488fe1274ee8b
fabio <fabiovianaqueiroz@gmail.com>
2019-12-29 23:21:23 +0000

    deleted unused array class and interface

--- src/MainCalculator.java
+++ src/MainCalculator.java
@@ -2,28 +2,7 @@
 public class MainCalculator {
 
 	public static void main(String[] args) 
-	{
-//		// Array
-//		System.out.println("ArrayList tests:");		
-//		ArrayLinearList numbers = new ArrayLinearList(5);
-//		
-//		for (int i = 0; i < 5; i++) 
-//		{
-//			numbers.add(i, i + 1);			
-//		}
-//		
-//		System.out.println(numbers.size());
-//		System.out.println(numbers.outputList());
-//		
-//		System.out.println(numbers.get(2));
-//		
-//		numbers.remove(2);
-//		
-//		System.out.println(numbers.outputList());
-//		System.out.println(numbers.get(2));
-//		System.out.println("_________________");		
-//		System.out.println("");
-//		
+	{	
 //		// ArrayStack
 //		System.out.println("Array Stack tests:");
 //				
